04925abe45a0820c2973c4880f994abb8ab31c99
Yiaaaaa <2365072806@qq.com>
2025-01-27 15:31:19 +0800

    Site updated: 2025-01-27 15:31:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Yia./静态路由/","Yia./RIP/","Yia./hello-world/"];function toRandomPost(){
+var posts=["2025/01/25/静态路由/","2025/01/27/RIP/","2025/01/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
