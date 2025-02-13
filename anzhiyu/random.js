a7024d3936728c73e6afcfc9adafa7ebfd3e924e
Yiaaaaa <2365072806@qq.com>
2025-02-13 17:26:59 +0800

    Site updated: 2025-02-13 17:26:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/25/静态路由/","2025/01/27/RIP/","2025/01/25/hello-world/"];function toRandomPost(){
+var posts=["2025/02/13/线性图/","2025/01/27/RIP/","2025/01/25/静态路由/","2025/01/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
